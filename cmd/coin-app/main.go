19064f3f82d3b5c853fee52b3bc281be3761433b
Rinat Sagitov <sagitov256@gmail.com>
2024-12-20 13:52:47 +0500

    feat: added run with logger

--- cmd/coin-app/main.go
+++ cmd/coin-app/main.go
@@ -2,6 +2,15 @@ package main
 
 import (
 	"coin-app/internal/config"
+	"coin-app/internal/lib/logger/handlers/slogpretty"
+	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -9,11 +18,45 @@ func main() {
 	cfg := config.MustLoad()
 	_ = cfg
 
-	// TODO: Init logger: slog
+	// Init logger: slog
+	log := setupLogger(cfg.Env)
+	log.Info("starting driver server", slog.String("env", cfg.Env))
+	log.Debug("debug messages are enabled")
 
 	// TODO: Init storage: postgresql
 
 	// TODO: Init router: chi, "chi render"
 
 	// TODO: Run server
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
 }
